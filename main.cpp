ca9b041dc5c55ab51921cc798da3e02fe2af9050
justinguto <163184050+justinguto@users.noreply.github.com>
2026-03-16 16:27:13 -0700

    Refactor rainfall calculation and input/output handling

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
+
 int main()
 {
-  string month1, month2, month3;
-  double rainfall1, rainfall2, rainfall3;
-  double avg;
+    string month1, month2, month3;
+    double rainfall1, rainfall2, rainfall3, avg;
 
-  // TODO: Read three months and their rainfall values from standard input
+    
+    cin >> month1 >> rainfall1;
+    cin >> month2 >> rainfall2;
+    cin >> month3 >> rainfall3;
 
-  // TODO: Calculate the average rainfall
+    avg = (rainfall1 + rainfall2 + rainfall3) / 3.0;
 
-  cout << fixed << setprecision(2);
-  // TODO: Print the result in the required format
+    cout << fixed << setprecision(2);
+
+    cout << "The average rainfall for "
+         << month1 << ", "
+         << month2 << ", and "
+         << month3
+         << " is " << avg << endl;
+
+    return 0;
 }
